0d703165b985e0da3cf1ca4263d7b79f76eb4828
Alfredo Valbuena <almavalo11@gmail.com>
2020-01-22 15:00:26 -0430

    Actualizacion Proyecto Final

--- angularCrud/src/app/app.component.ts
+++ angularCrud/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCrud';
+
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Alfredo', country: 'Venezuela' },
+    { id: 2, name: 'Wilson', country: 'Chile' },
+    { id: 3, name: 'Saul', country: 'USA' }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id == 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    } else {
+      this.selectedEmployee = new Employee();
+    }
+  }
+
+  openEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  delete() {
+    if (confirm('Esta seguro de eliminar el usuario')) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee)
+      this.selectedEmployee = new Employee();
+    }
+
+  }
+
+
+
+
 }
